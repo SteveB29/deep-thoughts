2a25f6f4afcb99530cf50e1cdad8e8a5ad46ed12
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-06 22:34:39 -0500

    added add user and login mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,7 +4,7 @@ const { gql } = require('apollo-server-express');
 // create out typeDefs
 const typeDefs = gql`
   type User {
-    _is: ID
+    _id: ID
     username: String
     email: String
     friendCount: Int
@@ -33,6 +33,11 @@ const typeDefs = gql`
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
